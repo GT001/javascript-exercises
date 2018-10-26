4bc939c609caaf386b3b3ecf2aa7c348b7cb834b
Ghassan Taha <ghassan.m.taha@gmail.com>
2018-10-25 23:30:34 -0600

    all exercises completed with all tests passed.

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,9 +1,39 @@
-function translate() {
+function translate(string) {
 	// body...
+	var vowel = ["a","e","i","o","u"];
+	var word = string.split(" ");
+	var result = [];
+	var i = 0;
+	var words_count = word.length;
+
+	while (i < words_count) {
+
+		if (vowel.indexOf(word[i].charAt(0)) < 0) {
+			//not a vowel
+			j = 1;
+			while (j < word[i].length){
+				if ( vowel.indexOf(word[i].charAt(j)) < 0) {
+					j += 1;
+				} else if (word[i].charAt(j) == "u" && word[i].charAt(j-1) == "q") {
+					j += 1;
+				} else {
+					result.push(word[i].substr(j) + word[i].substr(0,j) + "ay"); 
+					//console.log(word[i].substr(j) + word[i].substr(0,j) + "ay")
+					break;
+				}
+			}
+		} else {
+			
+			result.push(word[0] + "ay");
+			
+		}
+		i += 1;
+
+	}
+	return result.join(" ");
 }
 
 
 module.exports = {
 	translate
 }
-
